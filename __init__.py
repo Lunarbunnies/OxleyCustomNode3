19263103c0f7fa352b796e7076ca0efcafd41601
David Oxley <100420600+Lunarbunnies@users.noreply.github.com>
2024-02-28 18:55:09 +0000

    First upload

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .oxleycustomnode import SIMPLE_CUSTOM_NODE
+NODE_CLASS_MAPPINGS = { "oxleycustomnode" : OxleyCustomNode }
+NODE_DISPLAY_NAME_MAPPINGS = { "oxleycustomnode" : "Oxley Image inverter" }
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
---
+++ oxleycustomnode.py
@@ -0,0 +1,15 @@
+class OxleyCustomNode:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": { "image_in" : ("IMAGE", {}) },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("image_out",)
+    FUNCTION = "invert"
+    CATEGORY = "examples"
+
+    def invert(self, image_in):
+        image_out = 1 - image_in
+        return (image_out,)
