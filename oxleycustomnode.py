908b3a1eea20a4e0da8b1e52974a768a19b63225
David Oxley <100420600+Lunarbunnies@users.noreply.github.com>
2024-02-29 11:22:24 +0000

    Update oxleycustomnode.py

--- oxleycustomnode.py
+++ oxleycustomnode.py
@@ -1,3 +1,6 @@
+import requests
+from io import BytesIO
+
 class OxleyCustomNode:
     @classmethod
     def INPUT_TYPES(cls):
@@ -8,8 +11,37 @@ class OxleyCustomNode:
     RETURN_TYPES = ("IMAGE",)
     RETURN_NAMES = ("image_out",)
     FUNCTION = "invert"
-    CATEGORY = "utils"
+    CATEGORY = "oxley"
 
     def invert(self, image_in):
         image_out = 1 - image_in
         return (image_out,)
+
+
+class OxleyDownloadImageNode:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": { "url" : ("STRING", {}) },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("image_out",)
+
+    FUNCTION = "download_image"
+    CATEGORY = "oxley"
+
+    def download_image(self, url):
+        # Send a GET request to the URL
+        response = requests.get(url)
+        
+        # Raise an exception if the request was unsuccessful
+        response.raise_for_status()
+
+        # Convert the response content into a bytes-like object
+        image_bytes = BytesIO(response.content)
+
+        # Depending on your environment's way of handling images,
+        # you might directly return the bytes, or you might need
+        # to convert this to your environment's image format.
+        return (image_bytes,)
