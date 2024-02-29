cac6203b2068a6a537b5ed5b53c12d5c94fd0b9e
David Oxley <100420600+Lunarbunnies@users.noreply.github.com>
2024-02-29 11:21:29 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,16 @@
-from .oxleycustomnode import OxleyCustomNode
-NODE_CLASS_MAPPINGS = { "oxleycustomnode" : OxleyCustomNode }
-NODE_DISPLAY_NAME_MAPPINGS = { "oxleycustomnode" : "Oxley Image inverter" }
+from .oxleycustomnode import OxleyCustomNode, OxleyDownloadImageNode
+
+# Mapping of node class names to their corresponding classes
+NODE_CLASS_MAPPINGS = {
+    "oxleycustomnode": OxleyCustomNode,
+    "oxleydownloadimagenode": OxleyDownloadImageNode  # Add the new class here
+}
+
+# Mapping of node class names to their display names in the UI
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "oxleycustomnode": "Oxley Image Inverter",
+    "oxleydownloadimagenode": "Oxley Image Downloader"  # Add the display name for the new class
+}
+
+# List of symbols that are imported when 'from package import *' is used
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
