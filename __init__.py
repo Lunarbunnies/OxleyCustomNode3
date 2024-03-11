ee66809f79f8131d17b7f261c57d50fed11efadc
David Oxley <100420600+Lunarbunnies@users.noreply.github.com>
2024-03-11 00:52:29 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
-from .oxleycustomnode import OxleyCustomNode, OxleyDownloadImageNode, OxleyWebsocketDownloadImageNode, OxleyWebsocketReceiveJsonNode
+from .oxleycustomnode import OxleyCustomNode, OxleyDownloadImageNode, OxleyWebsocketDownloadImageNode, OxleyWebsocketReceiveJsonNode, OxleyWebsocketPushImageNode
 
 # Mapping of node class names to their corresponding classes
 NODE_CLASS_MAPPINGS = {
     "oxleycustomnode": OxleyCustomNode,
     "oxleydownloadimagenode": OxleyDownloadImageNode,
     "oxleywebsocketdownloadimagenode": OxleyWebsocketDownloadImageNode,
+    "oxleywebsocketpushimagenode": OxleyWebsocketPushImageNode,
     "oxleywebsocketreceivejsonnode": OxleyWebsocketReceiveJsonNode 
 }
 
@@ -13,6 +14,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "oxleycustomnode": "Oxley Image Inverter",
     "oxleydownloadimagenode": "Oxley Image Downloader", 
     "oxleywebsocketdownloadimagenode": "Oxley Image Websocket Downloader", 
+    "oxleywebsocketpushimagenode": "Oxley Image Websocket Uploader",     
     "oxleywebsocketreceivejsonnode": "Oxley Json Websocket Downloader" 
 }
 
