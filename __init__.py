9f1d8143c04225e3d4eedf3e0083b21f5b64c46a
David Oxley <100420600+Lunarbunnies@users.noreply.github.com>
2024-07-09 14:47:18 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .oxleycustomnode import OxleyCustomNode, OxleyDownloadImageNode, OxleyWebsocketDownloadImageNode, OxleyWebsocketReceiveJsonNode, OxleyWebsocketPushImageNode
+from .oxleycustomnode import OxleyCustomNode, OxleyDownloadImageNode, OxleyWebsocketDownloadImageNode, OxleyWebsocketReceiveJsonNode, OxleyWebsocketPushImageNode, OxleyAlternatorNode
 
 # Mapping of node class names to their corresponding classes
 NODE_CLASS_MAPPINGS = {
@@ -6,7 +6,8 @@ NODE_CLASS_MAPPINGS = {
     "oxleydownloadimagenode": OxleyDownloadImageNode,
     "oxleywebsocketdownloadimagenode": OxleyWebsocketDownloadImageNode,
     "oxleywebsocketpushimagenode": OxleyWebsocketPushImageNode,
-    "oxleywebsocketreceivejsonnode": OxleyWebsocketReceiveJsonNode 
+    "oxleywebsocketreceivejsonnode": OxleyWebsocketReceiveJsonNode,
+    "oxleyalternatornode": OxleyAlternatorNode 
 }
 
 # Mapping of node class names to their display names in the UI
@@ -15,7 +16,8 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "oxleydownloadimagenode": "Oxley Image Downloader", 
     "oxleywebsocketdownloadimagenode": "Oxley Image Websocket Downloader", 
     "oxleywebsocketpushimagenode": "Oxley Image Websocket Uploader",     
-    "oxleywebsocketreceivejsonnode": "Oxley Json Websocket Downloader" 
+    "oxleywebsocketreceivejsonnode": "Oxley Json Websocket Downloader",
+    "oxleyalternatornode": "Oxley Alternator" 
 }
 
 # List of symbols that are imported when 'from package import *' is used
