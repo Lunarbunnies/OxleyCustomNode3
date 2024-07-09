692d6f4ec47840508f039b67973c7ac91463a3da
David Oxley <100420600+Lunarbunnies@users.noreply.github.com>
2024-07-09 14:56:00 +0100

    Update oxleycustomnode.py

--- oxleycustomnode.py
+++ oxleycustomnode.py
@@ -25,19 +25,16 @@ import base64
 from datetime import datetime, timedelta
 
 def get_latest_message(ws):
-    latest_message = None
     try:
-        while True:
-            message = ws.recv()
-            latest_message = message
+        message = ws.recv()
+        return message
     except WebSocketTimeoutException:
-        pass  # No more messages, exited the loop normally
+        return None  # No message received in the given timeout
     except Exception as e:
         print(f"An error occurred: {e}")
         if ws:
             ws.close()
         return -1  # Return -1 on other exceptions, indicating an error
-    return latest_message
 
 class OxleyAlternatorNode:
     counter = 0
